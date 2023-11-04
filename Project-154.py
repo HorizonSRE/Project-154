a78b572ee026d7bd2dab74c1646a6b1598d3a58c
HorizonSRE <106289299+HorizonSRE@users.noreply.github.com>
2023-11-04 11:29:02 -0700

    Add files via upload

---
+++ Project-154.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+data=[[4,10,10,4,10],
+      [4,10,4,10,10],
+      [4,4,10,10,10],
+      [4,10,4,10,10],
+      [4,10,10,4,10]
+      ]
+plt.imshow(data, cmap="hot")
+plt.show
